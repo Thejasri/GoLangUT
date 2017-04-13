5f8b03b129366eb5f96a43e003a5027a60db446e
T.Thejasri <thejasri45@gmail.com>
2017-04-13 19:29:28 +0530

    Create mypackage_test.go

---
+++ mypackage_test.go
@@ -0,0 +1,17 @@
+package mypackage_test
+import (
+"testing"
+"github.com/matryer/mypackage"
+)
+
+func TestCount(t *testing.T) {
+if mypackage.Count() != 1 {
+t.Error("expected 1")
+}
+if mypackage.Count() != 2 {
+t.Error("expected 2")
+}
+if mypackage.Count() !=3 {
+t.Error("expected 3")
+}
+}
